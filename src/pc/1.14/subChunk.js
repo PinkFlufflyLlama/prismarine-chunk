0cb82b5fbd0e2c485fca8d632493f62fb88405c8
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-03-03 21:03:32 +0000

    Added subChunk class

---
+++ src/pc/1.14/subChunk.js
@@ -0,0 +1,11 @@
+const subChunkHeight = 16;
+const subChunkWidth = 16;
+const subChunkLength = 16;
+
+// In the previous versions, the developers used a 1D array and used a function to make it act like a 3D array. Is there any benefit for doing that?
+
+// I'm going to choose to use a 3D array because I cant see any major reason why not to.
+
+class SubChunk {
+
+}
